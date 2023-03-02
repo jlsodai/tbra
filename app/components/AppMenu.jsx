96ac70dd77fb5480e8417e74f7ded4b538f46ea6
Jules Maestro <jlsodai@gmail.com>
2023-03-02 16:17:15 +0000

    add Appmenu and menu components

---
+++ app/components/AppMenu.jsx
@@ -0,0 +1,13 @@
+const AppMenu = () => {
+	return (
+		<>
+			<a href="#" className="hover:text-purple-600 mr-4">
+				Login
+			</a>
+			<a href="#" className="btn">
+				Sign Up
+			</a>
+		</>
+	);
+};
+export default AppMenu;
---
+++ app/components/Menu.jsx
@@ -0,0 +1,25 @@
+const Menu = () => {
+	return (
+		<>
+			<a className="menulink" href="#">
+				About Us
+			</a>
+			<a className="menulink" href="#">
+				Board Roles
+			</a>
+			<a className="menulink" href="#">
+				Executive Development
+			</a>
+			<a className="menulink" href="#">
+				Events
+			</a>
+			<a className="menulink" href="#">
+				Partners
+			</a>
+			<a className="menulink" href="#">
+				Research
+			</a>
+		</>
+	);
+};
+export default Menu;
