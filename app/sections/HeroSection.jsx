7acc513a2384c321bba5b8bc4bbba2ea6c6747c4
Jules Maestro <jlsodai@gmail.com>
2023-03-02 16:17:27 +0000

    add Hero and Navigation Components

---
+++ app/sections/HeroSection.jsx
@@ -0,0 +1,25 @@
+const HeroSection = () => {
+	return (
+		<div className="bg-darkblue">
+			<div className="flex container min-h-[calc(100dvh-75px)] mx-auto px-4 justify-center items-center">
+				<div>
+					<h1 className="text-gold-900 text-6xl">
+						Society thrives when women are equal decision makers.
+					</h1>
+					<p className="mt-4">
+						TheBoardroom Africa is Africa's leading private
+						membership network that promotes exceptional female
+						talent to boards and executive teams while transforming
+						the presence and influence of African women on a global
+						stage.
+					</p>
+					<br />
+					<a href="#" className="btn">
+						Sign Up
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default HeroSection;
