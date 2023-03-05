1b4866ba5aae226fcabb55b222877310fbd55814
Jules Maestro <jlsodai@gmail.com>
2023-03-05 20:25:20 +0000

    work on navigation responsiveness

--- app/components/AppMenu.jsx
+++ app/components/AppMenu.jsx
@@ -1,13 +1,13 @@
-const AppMenu = () => {
+const AppMenu = ({ className }) => {
 	return (
-		<>
+		<div className={className}>
 			<a href="#" className="hover:text-gold mr-4">
 				Login
 			</a>
 			<a href="#" className="btn">
 				Sign Up
 			</a>
-		</>
+		</div>
 	);
 };
 export default AppMenu;
