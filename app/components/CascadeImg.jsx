65001adaeb4a6ddde80cb7370c34a800f6898018
Jules Maestro <jlsodai@gmail.com>
2023-03-05 19:33:42 +0000

    add negative, top props to Cacade Img component

--- app/components/CascadeImg.jsx
+++ app/components/CascadeImg.jsx
@@ -1,15 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
-const CascadeImg = ({ src, position = "left" }) => {
+const CascadeImg = ({
+	src,
+	top = false,
+	position = "left",
+	negative = false,
+}) => {
 	return (
 		<div
 			className={`imgbg relative w-full before:content-[''] before:absolute before:top-0 before:bottom-0 before:w-[1000%] before:bg-gold mb-[70px] ${
 				position === "left" ? "before:right-[50%]" : "before:left-[50%]"
-			}`}
+			} ${negative ? "before:h-[calc(100%+70px)]" : ""}`}
 		>
 			<img
 				src={src}
 				alt="Mask Image"
-				className={`top-20 relative ${
+				className={`${top ? "" : "top-[70px]"} relative ${
 					position === "left" ? "mr-[100px]" : "ml-[100px]"
 				}`}
 			/>
