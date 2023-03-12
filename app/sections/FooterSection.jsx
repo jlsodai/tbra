0d5b3285565fe9f7f3427c39f556e71d0f8d761c
Jules Maestro <jlsodai@gmail.com>
2023-03-12 18:55:55 +0000

    change text

--- app/sections/FooterSection.jsx
+++ app/sections/FooterSection.jsx
@@ -5,12 +5,24 @@ const FooterSection = () => {
 		<>
 			<section className="mt-10 bg-gray-50 py-10">
 				<div className="container">
-					<h1 className="text-darkgray text-center">Our Impact</h1>
+					<h1 className="text-dusk text-center">Our Impact</h1>
 					<div className="grid grid-cols-4 gap-10 mx-20 mt-10">
-						<Stats stat="14" info="Valentine's Day Cake Recipes" />
-						<Stats stat="14" info="Valentine's Day Cake Recipes" />
-						<Stats stat="14" info="Valentine's Day Cake Recipes" />
-						<Stats stat="14" info="Valentine's Day Cake Recipes" />
+						<Stats
+							stat="14"
+							info="Important statistics displayed here"
+						/>
+						<Stats
+							stat="14"
+							info="Important statistics displayed here"
+						/>
+						<Stats
+							stat="14"
+							info="Important statistics displayed here"
+						/>
+						<Stats
+							stat="14"
+							info="Important statistics displayed here"
+						/>
 					</div>
 				</div>
 			</section>
@@ -18,7 +30,7 @@ const FooterSection = () => {
 			<section className="py-10">
 				<div className="container">
 					<div className="text-center mx-auto max-w-sm">
-						<h2 className="text-darkgray text-center inline-block mr-8 mt-4">
+						<h2 className="text-dusk text-center inline-block mr-8 mt-4">
 							Join Us
 						</h2>
 						<TBRAKey size="sm" />
