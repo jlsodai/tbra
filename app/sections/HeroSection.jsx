3109e433e66a0923f3d67aa9f5e7d5804011ab6d
Jules Maestro <jlsodai@gmail.com>
2023-03-12 19:12:17 +0000

    change hero section text and background colors, add border bottom

--- app/sections/HeroSection.jsx
+++ app/sections/HeroSection.jsx
@@ -1,26 +1,29 @@
+/* eslint-disable @next/next/no-img-element */
 const HeroSection = () => {
 	return (
-		<div className="bg-darkblue">
+		<div className="bg-darkblue border-b-8 border-dawn">
 			<div className="flex container min-h-[600px] relative px-4 justify-center items-center ">
 				<img
 					src="/images/white-key.png"
 					className="absolute left-0 h-[75%] -z-1 opacity-5"
 					alt=""
 				/>
-				<div className="z-10">
+				<div className="z-10 text-xl flex flex-col gap-4">
 					<h1 className="text-mustard">
 						Society thrives when women are equal decision makers.
 					</h1>
-					<p className="mt-4 text-mustard-300 md:max-w-[65%]">
+					<p className=" text-dawn md:max-w-[65%]">
 						TheBoardroom Africa is Africa's leading private
 						membership network that promotes exceptional female
 						talent to boards and executive teams while transforming
 						the presence and influence of African women on a global
 						stage.
 					</p>
-					<a href="#" className="btn mt-4 text-white">
-						Join Now
-					</a>
+					<div>
+						<a href="#" className="btn mt-4 bg-dawn font-normal">
+							Join Now
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
