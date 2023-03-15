d8a5c63b9063939865a6264bd28ed1b3abee16f3
Jules Maestro <jlsodai@gmail.com>
2023-03-15 16:00:55 +0000

    add placeholder to permitted remote images

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+	experimental: {
+		appDir: true,
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "placeholder.com",
+				port: "",
+				pathname: "/*",
+			},
+		],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
