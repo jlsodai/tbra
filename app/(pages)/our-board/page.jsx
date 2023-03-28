2bd741a542cba971aa7546e2577e43e3a5b3414d
Jules Maestro <jlsodai@gmail.com>
2023-03-28 09:41:59 +0000

    duplicate team page for our board

--- app/(pages)/our-board/page.jsx
+++ app/(pages)/our-board/page.jsx
@@ -7,16 +7,10 @@ import SectionTitle from "@/app/components/SectionTitle";
 const page = () => {
 	return (
 		<>
-			<HeaderTitle title="Our Team" />
-
+			<HeaderTitle title="Our Board" />
 			<section className="mt-16" data-aos="fade-up">
 				<div className="container md:px-44">
 					<SectionTitle title="Our Board" />
-				</div>
-			</section>
-
-			<section>
-				<div className="container">
 					<div className="grid grid-cols-3 gap-10 mt-10">
 						<TeamCard teamInfo={teams} />
 					</div>
