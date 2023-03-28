deab0071cc8553b699e775c5142c587117251663
Jules Maestro <jlsodai@gmail.com>
2023-03-28 09:38:41 +0000

    redesign form input... simpler

--- app/ui/FormInput.jsx
+++ app/ui/FormInput.jsx
@@ -1,9 +1,9 @@
 const FormInput = ({ label, type = "text", required = false }) => {
 	return (
-		<div class="md:flex md:items-center mt-4">
+		<div className="md:flex md:items-center mt-4">
 			<div className="w-full">
 				<input
-					className="bg-gray-200 appearance-none border-2 border-gray-200 w-full py-2 px-4 focus:outline-none focus:bg-white "
+					className="bg-dawn-200 appearance-none border-2 border-dawn-200 w-full py-2 px-4 focus:outline-none focus:bg-white "
 					type={type}
 					placeholder={label}
 				/>
