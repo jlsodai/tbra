d107401eeed40891f131fa59047eaadcefcfdccc
Jules Maestro <jlsodai@gmail.com>
2023-03-28 09:39:42 +0000

    update footer section

--- app/sections/FooterMenu.jsx
+++ app/sections/FooterMenu.jsx
@@ -5,15 +5,14 @@ import { FaLinkedin } from "react-icons/fa";
 
 const FooterMenu = () => {
 	return (
-		<footer className="mt-16 px-4 divide-y bg-dawn">
-			<div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
+		<footer className="mt-16 px-4 divide-y bg-tender text-dawn-200 bigborder">
+			<div className="container flex flex-col justify-between py-20 mx-auto space-y-8 lg:flex-row lg:space-y-0">
 				<div className="lg:w-1/5">
 					<a
-						rel="noopener noreferrer"
 						href="#"
 						className="flex justify-center space-x-3 lg:justify-start"
 					>
-						<div className="flex items-center justify-center w-12 h-12 rounded-full dark:bg-violet-400">
+						<div className="flex items-center justify-center w-12 h-12 rounded-full">
 							<img
 								src="https://theboardroomafrica.com/wp-content/themes/tbra/assets/images/logo-secondary.svg"
 								alt="logo"
@@ -23,7 +22,7 @@ const FooterMenu = () => {
 				</div>
 				<div className="grid grid-cols-2 gap-x-3 gap-y-8 lg:w-4/5 sm:grid-cols-4">
 					<div className="space-y-3">
-						<ul className="space-y-1">
+						<ul className="space-y-3">
 							<li>
 								<a href="#">About TBrA</a>
 							</li>
@@ -39,7 +38,7 @@ const FooterMenu = () => {
 						</ul>
 					</div>
 					<div className="space-y-3">
-						<ul className="space-y-1">
+						<ul className="space-y-3">
 							<li>
 								<a href="#">Contact us</a>
 							</li>
@@ -55,7 +54,7 @@ const FooterMenu = () => {
 						</ul>
 					</div>
 					<div className="space-y-3">
-						<ul className="space-y-1">
+						<ul className="space-y-3">
 							<li>
 								<a href="#">Board Diversity Charter</a>
 							</li>
@@ -70,32 +69,28 @@ const FooterMenu = () => {
 							</li>
 						</ul>
 					</div>
-					<div className="space-y-3">
-						<div className="uppercase dark:text-gray-50">
-							Follow us
-						</div>
-						<div className="flex justify-start space-x-3">
+					<div className="space-y-3 justify-self-center">
+						<div className="uppercase font-bold">Follow us</div>
+						<div className="flex justify-start space-x">
 							<a
-								rel="noopener noreferrer"
 								href="#"
 								title="Twitter"
 								className="flex items-center p-1"
 							>
-								<FaTwitterSquare />
+								<FaTwitterSquare className="text-2xl" />
 							</a>
 							<a
-								rel="noopener noreferrer"
 								href="#"
 								title="Instagram"
 								className="flex items-center p-1"
 							>
-								<FaLinkedin />
+								<FaLinkedin className="text-2xl" />
 							</a>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div className="py-6 text-center dark:text-gray-400">
+			<div className="py-6 text-center">
 				© {new Date().getFullYear()} TheBoardroom Africa. All rights
 				reserved.
 			</div>
