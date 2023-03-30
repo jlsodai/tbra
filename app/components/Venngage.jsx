7f8a5dfd0d56ca33c1d00cd52be41f69ee039c8a
Jules Maestro <jlsodai@gmail.com>
2023-03-30 07:43:30 +0000

    add venngage page... incomplete

--- app/components/Venngage.jsx
+++ app/components/Venngage.jsx
@@ -1,16 +1,12 @@
-import Script from "next/script";
 const Venngage = () => {
 	return (
 		<>
-			<Script src="https://infograph.venngage.com/js/embed/v1/embed.js" />
 			<div
-				id="venngage"
-				data-vg-id="nlKnbkFq2FI"
-				data-title="Copy - Copy - Ghana 2019B"
-				data-w="816"
-				data-h="1056"
-				data-multipage="true"
-			></div>
+				className="mt-16"
+				dangerouslySetInnerHTML={{
+					__html: '<script src="https://infograph.venngage.com/js/embed/v1/embed.js" data-vg-id="uhv27zuijIk" data-title="2022 Something Here" data-w="816" data-h="1056" data-multipage="true" data-f="false"></script>',
+				}}
+			/>
 		</>
 	);
 };
