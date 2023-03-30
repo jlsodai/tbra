abe88da72e23d4cd0462fae26a7d83b6333c61ec
Jules Maestro <jlsodai@gmail.com>
2023-03-30 07:46:31 +0000

    modify article

--- app/components/Article.jsx
+++ app/components/Article.jsx
@@ -5,39 +5,34 @@ const Article = ({ posts }) => {
 		<>
 			{posts.map((post, i) => (
 				<article
-					key={post.id}
+					key={i}
 					className="flex max-w-xl flex-col items-start justify-between rounded border p-4 hover:shadow-lg"
 				>
-					<img src={post.imageUrl} className="mb-4" alt="" />
-					<div className="flex items-center gap-x-4 text-xs">
-						<time
-							dateTime={post.datetime}
-							className="text-gray-500"
-						>
-							{post.date}
-						</time>
-						<a
-							href={post.category.href}
-							className="relative z-10 rounded-full bg-gray-50 py-1.5 px-3 font-medium text-gray-600 hover:bg-gray-100"
-						>
-							{post.category.title}
-						</a>
+					<img
+						src={post.featuredImage?.node?.sourceUrl}
+						className="mb-4"
+						alt=""
+					/>
+					<div className="flex items-center gap-x-2 text-xs">
+						<p className="rounded-full bg-dawn py-1.5 px-3 font-medium text-white capitalize">
+							{post.publicationTypes?.nodes[0]?.name}
+						</p>
+						<p className="rounded-full bg-tender py-1.5 px-3 font-medium text-white capitalize">
+							{post.publicationYears?.nodes[0]?.name}
+						</p>
 					</div>
-					<div className="group relative">
-						<h3 className="mt-2 text-2xl font-semibold leading-6 group-hover:text-gray-600">
-							<a href={post.href}>
-								<span className="absolute inset-0" />
-								{post.title}
-							</a>
+					<div>
+						<h3 className="mt-4 text-2xl font-semibold leading-6 group-hover:text-gray-600 mb-0">
+							<a href={`/research/${post.slug}`}>{post.title}</a>
 						</h3>
-						{post.description && (
+						{/* {post.content && (
 							<p className="mt-2 leading-6 text-gray-600 line-clamp-3">
-								{post.description}
+								{post.content}
 							</p>
-						)}
+						)} */}
 						<TbraLink
 							linkTitle="Read More"
-							url={`/research/${post.href}`}
+							url={`/research/${post.slug}`}
 						/>
 					</div>
 				</article>
