312c277442c72da565fe45c4a1aa8ec7e5b6642c
Jules Maestro <jlsodai@gmail.com>
2023-03-30 07:49:07 +0000

    modify research pages

--- app/(pages)/research/[slug]/page.jsx
+++ app/(pages)/research/[slug]/page.jsx
@@ -4,17 +4,7 @@ const page = () => {
 	return (
 		<>
 			<HeaderTitle title="Research Post Title" />
-			<section>
-				<div className="container my-16">
-					<img
-						src="https://via.placeholder.com/1000x1000"
-						className="mx-auto"
-						alt=""
-					/>
-				</div>
-
-				<Venngage />
-			</section>
+			<section>{/* <Venngage /> */}</section>
 		</>
 	);
 };
--- app/(pages)/research/page.jsx
+++ app/(pages)/research/page.jsx
@@ -2,15 +2,21 @@ import Article from "@/app/components/Article";
 import HeaderTitle from "@/app/sections/HeaderTitle";
 import { posts } from "@/app/utils/options";
 import Pagination from "@/app/components/Pagination";
+import {
+	publications,
+	sortedPublications,
+	filterPublicationYear,
+} from "@/app/lib/publications";
 
 export default function Example() {
+	console.log(filterPublicationYear(2022));
 	return (
 		<>
 			<HeaderTitle title="Research" />
 			<section className="my-16">
 				<div className="container">
 					<div className="mx-auto grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 lg:max-w-none lg:grid-cols-3">
-						<Article posts={posts} />
+						{/* <Article posts={sortedPublications} /> */}
 					</div>
 					<Pagination />
 				</div>
