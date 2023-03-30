25622128986615d235efc89ab5ef4e3a2e9ce7d2
Jules Maestro <jlsodai@gmail.com>
2023-03-30 08:49:35 +0000

    add esg to programs lib

---
+++ app/lib/programmes.js
@@ -0,0 +1,91 @@
+export const esgProgramme = {
+	outline: [
+		{
+			title: "Introduction to ESG: Past and Present",
+			content:
+				"This introductory session explores the historical role corporations have played in advancing social good, from corporate social responsibility to corporate philanthropy and impact. You will learn about the journey from Socially Responsible Investment (SRI) to environmental, social and governance (ESG) criteria. Participants will also be introduced to the roles and responsibilities of different actors in the system, from investors to governments and nonprofits.",
+		},
+		{
+			title: "Global Trends Driving the Sustainability Agenda",
+			content:
+				"ESG is not only in the mainstream, but it also shapes the market. The session probes the factors driving growing momentum around the ESG agenda. You will dive into the role of key stakeholders, including investors, consumers, the media, regulators and more, in accelerating pressure for disclosure and action.",
+		},
+		{
+			title: "The Role of Regulation in Advancing ESG",
+			content:
+				"Regulatory scrutiny of ESG is on the rise as a growing number of countries seem to encourage companies to report on their environmental and social risk. You will learn what's driving new regulations and reporting standards. By the end of this session, you will understand the “alphabet soup” of regulation around ESG and how it impacts corporate strategy and compliance requirements.",
+		},
+		{
+			title: "ESG and Value Creation",
+			content:
+				"Research shows that thinking and acting proactively on ESG can generate commercial, reputation and societal returns. Your ESG journey should take a holistic approach that considers how to optimise value creation. You will delve into how a strong ESG proposition can accelerate top-line growth, reduce costs, garner government support, attract talent and enhance long-term returns.",
+		},
+		{
+			title: "Managing and Assessing ESG Risks and Opportunities",
+			content:
+				"ESG variables can have a considerable impact on a company's long-term sustainability and profitability. Overlooking these risks can result in financial and reputation losses. This session breaks down key considerations when assessing and managing ESG risks to help you understand how to move from a compliance-driven approach to a proactive risk reduction strategy. You will also learn about best practices when designing an effective Environmental and Social Management System (ESMS).",
+		},
+		{
+			title: "The Business Implications of ESG Issues",
+			content:
+				"Having understood key ESG considerations, companies need to take action. To be successful, you must adopt a practical approach. You will learn about how to design an ESG action plan built around key ESG themes that are material for most businesses across industries whilst taking into account the “5 signals”: regulation, litigation, clients, employees/investors and activism. The session will also explore practical approaches focused on materiality and competitive advantage, developed by leading academics from London Business School and New York University.",
+		},
+		{
+			title: "Leadership and Culture",
+			content:
+				"This session focuses on leadership and culture in the context of agenda setting, challenging the status quo, and shaping mindsets and the organisation's culture.",
+		},
+		{
+			title: "Role of the Board in Setting and Overseeing the Implementation of the ESG agenda",
+			content:
+				"There are extensive legal, academic and investor arguments for and against incorporating ESG into decision-making. This session will explore contemporary debates around the evidence and rationale around addressing the material impacts of ESG in corporate strategy.",
+		},
+		{
+			title: "Determining an Effective ESG Governance Model: Board Composition and Structure",
+			content:
+				'There is no "one-size-fits-all" approach for determining how ESG will be overseen by the board and its committees. The session will cover the factors to be considered in determining an effective board governance model around ESG. Key topics addressed include business context, business strategy, resource availability and efficiency, risk profile and more. You will also learn about board diversity from an ESG perspective as well as how to optimise ESG oversight models in the boardroom and how to consider ESG data in board decision-making.',
+		},
+		{
+			title: "Deep Dive Sessions on Environmental and Social Issues",
+			content:
+				"You will engage in two deep dive sessions focused on managing environmental and social challenges. Each session will introduce a case study of a company navigating a major environmental and social issue, respectively. Through a practical, discussion-based session, you will apply what you've learned about effective ESG strategy to develop approaches for crisis mitigation, risk management and future value creation.",
+		},
+		{
+			title: "ESG Reporting and How to Avoid/Spot Greenwashing",
+			content:
+				"It can be tricky to parse out the “sense and nonsense” of ESG reporting frameworks, which are numerous and ever-evolving. You will be introduced to frameworks and best practices around developing an ESG narrative so that you are equipped to prioritise factors and metrics that matter. By the end of this session, you will be able to: <br/><ul><li>Determine - as a potential client - which ESG funds are 'genuinely' ESG</li><li>Better articulate - as an asset manager - why your ESG fund is 'genuinely' ESG</li><li>Better articulate - as a manager - how your company defines ESG / CSR</li></ul>",
+		},
+	],
+	instructors: [
+		{
+			name: "Rosalind Kainyah MBE",
+			imageUrl:
+				"https://theboardroomafrica.com/wp-content/uploads/2022/10/rosalind_pic2.jpg",
+			profile: [
+				" Rosalind Kainyah MBE is an authority on Sustainability and responsible business with over 30 years of combined legal, international, executive and board level experience. She is a trusted advisor to Boards and Senior Executives of companies on leveraging opportunities and managing Sustainability related risk. Rosalind has advised companies across various sectors, including the extractives industry, energy, private equity, financial services, healthcare, agriculture, consumer goods, logistics, manufacturing and technology.",
+				"Rosalind is also an experienced non-executive director.  She currently serves on the boards of discoverIE plc, Gem Diamonds Limited, CalBank Plc, Lekela Power and BTE Renewables. In the not-for-profit sector, she is a trustee and Vice Chairperson of the Africa Gifted Foundation and a Board member of Results for Development (R4D). She has previously served on the Boards of Aker Energy, the Ghana Food &Drugs Authority and The Africa-America Institute.",
+				"Rosalind holds a Bachelor's degree in English from the University of Ghana, a Bachelor of Laws degree from the University of London and a Master of Laws degree from University College, London. She is a member of the Bar of England and Wales and the Chartered Institute of Arbitrators.",
+			],
+		},
+		{
+			name: "Jacqueline Musiitwa",
+			imageUrl:
+				"https://theboardroomafrica.com/wp-content/uploads/2022/10/jacqueline.jpg",
+			profile: [
+				"Ms Jacqueline Musiitwa is an international attorney specialising in business transformation, innovation and sustainability.  Jacqueline has over 17 years' experience in leading legal and corporate relations functions in financial services and the industrial sectors.  She is a member of faculty at Georgetown University.",
+				"Jacqueline sits on the boards of Bushveld Minerals (AIM: BMN), PPC Zimbabwe Limited  and is the Chair of the New York State Bar Association's Energy and Natural Resources Committee. She has experience as a board chair and chairing governance and risk committees.  She previously served in various leadership capacities at Rio Tinto, the Trade and Development Bank (TDB Group), World Trade Organization.",
+				"She holds a Juris Doctor from the University of Melbourne Law School and a Bachelor of Arts from Davidson College.  She holds a Certificate of Company Direction from the Institute of Directors UK.",
+			],
+		},
+		{
+			name: "Harald Walkate",
+			imageUrl:
+				"https://theboardroomafrica.com/wp-content/uploads/2022/10/harald.jpg",
+			profile: [
+				"Harald Walkate is the founder of Finding Ways Ahead, an independent ESG and impact investment advisory firm. From 2019 to 2021 he was the Head of Responsible Investment (ESG) and member of the Executive Committee at Natixis Investment Managers. Prior to that, he worked for Aegon for fifteen years in different capacities, lastly as Global Head of Responsible Investment at Aegon Asset Management.",
+				"His work in ESG has included working with portfolio managers and analysts to integrate ESG considerations into investments, active ownership, policy and organizational issues, and identifying positive impact investment opportunities within different asset classes, with a particular focus on blended finance.",
+				"Harald is a Senior Fellow with the University of Zurich Center for Sustainable Finance and Private Wealth (CSP) and a regular contributor to University of Cambridge Institute for Sustainability Leadership (CISL) programs. He was also an advisor to the Impact Management Project and on the board of the AIF Center for ESG and Sustainable Finance.",
+			],
+		},
+	],
+};
