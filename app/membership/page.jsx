63ac8c5f24f1305042b8805c24eef52ef82501d9
Jules Maestro <jlsodai@gmail.com>
2023-03-30 15:24:49 +0000

    add background images to membership page

--- app/membership/page.jsx
+++ app/membership/page.jsx
@@ -27,7 +27,7 @@ const MembershipHome = () => {
 						className="absolute left-0 h-[75%] -z-1 opacity-5"
 						alt=""
 					/>
-					<div className="grid grid-cols-2 z-10 items-center">
+					<div className="grid grid-cols-2 gap-20 items-center">
 						<div>
 							<h4 className="text-mustard text-6xl font-serif">
 								Powerful community
@@ -44,14 +44,21 @@ const MembershipHome = () => {
 								Uber and more.
 							</p>
 						</div>
-						<Image
-							src="https://placeholder.com/300x400"
-							width="300"
-							height="400"
+						<img
+							src="https://res.cloudinary.com/tbra/image/upload/v1680184362/community_tnruqd.png"
 							alt=""
 							className="mx-auto"
 						/>
 					</div>
+				</div>
+			</section>
+
+			<section className="bg-[#384159] py-16">
+				<div className="container flex flex-col">
+					<h3 className="text-center text-dawn font-libreb text-4xl md:mx-32">
+						Our members come from some of Africa&apos;s leading
+						companies and organisations
+					</h3>
 					<TrustedBy
 						logos={communityLogos}
 						className="community-logos"
@@ -59,7 +66,11 @@ const MembershipHome = () => {
 				</div>
 			</section>
 
-			<section className="bg-[#5D6579]">
+			<section
+				style={bgStyle(
+					"https://tbratest.my.canva.site/membership-learn-more-page/images/b8f20935578f5ba1b8b182cd33fff00a.jpg"
+				)}
+			>
 				<div className="flex container min-h-screen relative px-4 items-center">
 					<img
 						src="/images/white-key.png"
@@ -90,14 +101,18 @@ const MembershipHome = () => {
 				</div>
 			</section>
 
-			<section className="bg-[#222344]">
-				<div className="flex container min-h-screen relative px-4 items-center">
+			<section
+				style={bgStyle(
+					"https://tbratest.my.canva.site/membership-learn-more-page/images/a00b60f40dce31fa319e4dcd91c0d494.jpg"
+				)}
+			>
+				<div className="flex flex-col container min-h-screen relative px-4 justify-center">
 					<img
 						src="/images/white-key.png"
 						className="absolute left-0 h-[75%] -z-1 opacity-3"
 						alt=""
 					/>
-					<div className="grid grid-cols-2 z-10 py-28 text-white">
+					<div className="grid grid-cols-2 z-10 mb-28 text-white">
 						<div className="">
 							<h4 className="text-mustard text-6xl font-serif">
 								Group coaching
@@ -113,43 +128,22 @@ const MembershipHome = () => {
 						</div>
 						<div>&nbsp;</div>
 					</div>
-				</div>
-			</section>
-
-			<section className="bg-[#434460]">
-				<div className="flex container min-h-screen relative px-4 items-center">
 					<img
-						src="/images/white-key.png"
-						className="absolute left-0 h-[75%] -z-1 opacity-3"
+						src="https://res.cloudinary.com/tbra/image/upload/v1680188246/gc_words_z1ycum.png"
 						alt=""
 					/>
-					<div className="grid grid-cols-2 z-10 py-28 text-white">
-						<div className="">
-							<h4 className="text-mustard text-6xl font-serif">
-								The tools you need to stay on top
-							</h4>
-							<p className="mt-4 font-halyard font-extralight text-3xl text-mustard">
-								Position yourself to stay on top.
-							</p>
-							<p className="mt-4">
-								Whether it&apos;s discovering how to position
-								yourself for the next major move, avoiding
-								getting stuck, or preparing yourself for
-								regional and global opportunities, there are
-								clear steps you can take to advance those
-								ambitions.
-							</p>
-							<p>
-								Your membership provides access to our range of
-								professional advancement tools.
-							</p>
-						</div>
-						<div>&nbsp;</div>
-					</div>
 				</div>
 			</section>
 
-			<section className="bg-[#6B6C82]">
+			<section
+				style={{
+					...bgStyle(
+						"https://tbratest.my.canva.site/membership-learn-more-page/images/66a99990b0237ba0bb45488d7fc4a6d8.jpg"
+					),
+					backgroundSize: "100%",
+					backgroundPosition: "0 -200px",
+				}}
+			>
 				<div className="flex container min-h-screen relative px-4 items-center">
 					<img
 						src="/images/white-key.png"
@@ -172,6 +166,44 @@ const MembershipHome = () => {
 							</p>
 						</div>
 						<div>&nbsp;</div>
+					</div>
+				</div>
+			</section>
+
+			<section className="bg-darkblue">
+				<div className="flex container min-h-screen relative px-4 items-center">
+					<img
+						src="/images/white-key.png"
+						className="absolute left-0 h-[75%] -z-1 opacity-3"
+						alt=""
+					/>
+					<div className="grid grid-cols-2 z-10 py-28 text-white items-center">
+						<div className="">
+							<h4 className="text-mustard text-6xl font-serif">
+								The tools you need to stay on top
+							</h4>
+							<p className="mt-4 font-halyard font-extralight text-3xl text-mustard">
+								Position yourself to stay on top.
+							</p>
+							<p className="mt-4">
+								Whether it&apos;s discovering how to position
+								yourself for the next major move, avoiding
+								getting stuck, or preparing yourself for
+								regional and global opportunities, there are
+								clear steps you can take to advance those
+								ambitions.
+							</p>
+							<p>
+								Your membership provides access to our range of
+								professional advancement tools.
+							</p>
+						</div>
+						<img
+							src="https://res.cloudinary.com/tbra/image/upload/v1680189640/mockup_fp3gab.png"
+							width="300px"
+							alt="membership mockup"
+							className="mx-auto"
+						/>
 					</div>
 				</div>
 			</section>
@@ -230,3 +262,8 @@ const MembershipHome = () => {
 	);
 };
 export default MembershipHome;
+
+const bgStyle = (bgImg, opacity = 90) => ({
+	background: `linear-gradient(rgba(32,41,69, 0.${opacity}), rgba(32,41,69, 0.${opacity})), url('${bgImg}')`,
+	backgroundSize: "cover",
+});
