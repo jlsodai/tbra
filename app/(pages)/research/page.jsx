169ae706e5572c80e5831ea504cb3ea0d0c97e2e
Jules Maestro <jlsodai@gmail.com>
2023-03-30 15:28:37 +0000

    change research page title to BDI

--- app/(pages)/research/page.jsx
+++ app/(pages)/research/page.jsx
@@ -12,7 +12,7 @@ export default function Example() {
 	console.log(filterPublicationYear(2022));
 	return (
 		<>
-			<HeaderTitle title="Research" />
+			<HeaderTitle title="Board Diversity Index" />
 			<section className="my-16">
 				<div className="container">
 					<div className="mx-auto grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 lg:max-w-none lg:grid-cols-3">
