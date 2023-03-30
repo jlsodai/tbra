f714e2c11df79ad8885dbffc3295bbaa8df5d2a3
Jules Maestro <jlsodai@gmail.com>
2023-03-30 15:44:03 +0000

    add alternative hero... implement on research page

--- app/(pages)/research/page.jsx
+++ app/(pages)/research/page.jsx
@@ -1,18 +1,29 @@
 import Article from "@/app/components/Article";
 import HeaderTitle from "@/app/sections/HeaderTitle";
-import { posts } from "@/app/utils/options";
 import Pagination from "@/app/components/Pagination";
 import {
 	publications,
 	sortedPublications,
 	filterPublicationYear,
 } from "@/app/lib/publications";
+import HeroAlt from "@/app/sections/HeroAlt";
 
 export default function Example() {
 	console.log(filterPublicationYear(2022));
 	return (
 		<>
-			<HeaderTitle title="Board Diversity Index" />
+			{/* <HeaderTitle title="Board Diversity Index" /> */}
+			<HeroAlt title="Board Diversity Index">
+				<p className="text-dawn font-light">
+					Since 2017, TheBoardroom Africa has mapped the composition
+					of Africa&apos;s listed boards to assess progress on gender
+					diversity across the region. The index tracks the number of
+					listed boards by country and identifies all board seats
+					filled by women while also monitoring the number of women in
+					Chair and other executive positions. Research to date covers
+					publicly listed boards in 11 African countries.
+				</p>
+			</HeroAlt>
 			<section className="my-16">
 				<div className="container">
 					<div className="mx-auto grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 lg:max-w-none lg:grid-cols-3">
---
+++ app/sections/HeroAlt.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+
+const HeroAlt = ({ title, children }) => {
+	return (
+		<section className="bg-darkblue bigborder" data-aos="fade-up">
+			<div className="flex container relative px-4 justify-center items-center ">
+				<img
+					src="/images/white-key.png"
+					className="absolute left-0 h-[75%] -z-1 opacity-4"
+					alt=""
+				/>
+				<div className="z-10 text-xl flex flex-col gap-6 my-28">
+					<h1 className="text-mustard">{title}</h1>
+					<div className="md:max-w-[65%]">
+						{children}
+						{/* <div className="text-center mt-10">
+							<Link
+								href="/membership"
+								className="btn bg-dawn font-normal"
+							>
+								Join Now
+							</Link>
+						</div> */}
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default HeroAlt;
