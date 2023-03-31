803d37aaf8bf8eb4388abc9d8d2d4ed744ad5f24
Jules Maestro <jlsodai@gmail.com>
2023-03-31 08:55:35 +0000

    fix program card

--- app/components/ProgramCard.jsx
+++ app/components/ProgramCard.jsx
@@ -1,36 +1,45 @@
 import { GoCalendar } from "react-icons/go";
 const ProgramCard = ({ programme }) => {
 	return (
-		<div className="border border-dusk-200 hover:shadow-lg rounded overflow-hidden md:flex">
+		<div className="border border-dusk-200 hover:shadow-lg overflow-hidden md:flex">
 			<div
-				className={`bg-cover w-56 bg-top`}
+				className={`bg-cover w-44 min-h-[185px] bg-top shrink-0`}
 				style={{ backgroundImage: `url(${programme.imageUrl})` }}
 			></div>
-			<div>
-				<div className="py-4 px-6">
-					<h3 className="font-halyard text-2xl mb-2">
-						{programme.title}
-					</h3>
+			<div className="py-4 px-6 flex flex-col justify-between">
+				<h3 className="font-halyard text-2xl mb-2">
+					{programme.title}
+				</h3>
+				<div>
 					<div className="flex items-center gap-2">
 						<GoCalendar className="text-2xl" />
 						<p>{programme.date}</p>
 					</div>
-					<div className="flex gap-6">
+
+					<div className="flex gap-6 mt-4">
 						{programme.learnMoreUrl && (
 							<a
 								href={programme.learnMoreUrl}
-								className="btn font-normal text-white mt-4"
+								className="btn font-normal text-white"
 							>
 								Learn More
 							</a>
 						)}
 						{programme.registerUrl && (
-							<a
-								href={programme.registerUrl}
-								className="btn font-normal bg-tender text-white mt-4"
-							>
-								Register Now
-							</a>
+							<>
+								{programme.sold ? (
+									<a className="btn-disabled font-normal cursor-not-allowed">
+										Sold Out
+									</a>
+								) : (
+									<a
+										href={programme.registerUrl}
+										className="btn font-normal bg-tender text-white"
+									>
+										Apply Now
+									</a>
+								)}
+							</>
 						)}
 					</div>
 				</div>
