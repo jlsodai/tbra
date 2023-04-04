55aa49698212ba943112d69b7b5f1b3e7c546d9d
Jules Maestro <jlsodai@gmail.com>
2023-04-04 16:27:21 +0000

    gs

---
+++ app/components/ProgrammeLogos.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
+const ProgrammeLogos = ({ logos = [], className }) => {
+	return (
+		<>
+			<div
+				className={`partnerLogos flex flex-wrap items-center justify-center mt-10 ${className}`}
+			>
+				{logos.map((img, i) => (
+					<img
+						key={i}
+						data-aos="fade-up"
+						src={img.logourl}
+						alt=""
+						className="aos-init aos-animate"
+					/>
+				))}
+			</div>
+		</>
+	);
+};
+export default ProgrammeLogos;
