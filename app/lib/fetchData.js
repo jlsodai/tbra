5b48ee66934c5dc8eb302aa3bc322a41d275d7f0
Jules Maestro <jlsodai@gmail.com>
2023-04-04 16:52:41 +0000

    disable cache

--- app/lib/fetchData.js
+++ app/lib/fetchData.js
@@ -1,7 +1,7 @@
 export async function fetchData(endpoint) {
 	const res = await fetch(
 		`${process.env.NEXT_PUBLIC_STRAPI_URL}/${endpoint}`,
-		{ next: { revalidate: 10 } }
+		{ cache: "no-store" }
 	);
 	if (!res.ok) {
 		throw new Error("Failed to fetch data");
