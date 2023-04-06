c1a86b05cbe1944481baf9583795412ce99350fd
Jules Maestro <jlsodai@gmail.com>
2023-04-06 13:55:19 +0000

    add open doors and esg certificate application pages

---
+++ app/(form-pages)/esg-certificate-application/page.jsx
@@ -0,0 +1,15 @@
+import PaperForm from "@/app/components/PaperForm";
+import HeaderTitle from "@/app/sections/HeaderTitle";
+
+const page = () => {
+	return (
+		<>
+			<HeaderTitle title="ESG Certification Application." />
+			<section className="container my-10" data-aos="fade-up">
+				<PaperForm formSlug="esgcertificateapplicationform" />;
+			</section>
+		</>
+	);
+};
+
+export default page;
---
+++ app/(form-pages)/open-doors-application/page.jsx
@@ -0,0 +1,15 @@
+import PaperForm from "@/app/components/PaperForm";
+import HeaderTitle from "@/app/sections/HeaderTitle";
+
+const page = () => {
+	return (
+		<>
+			<HeaderTitle title="Open Doors Application." />
+			<section className="container my-10" data-aos="fade-up">
+				<PaperForm formSlug="opendoorsapplicationform" />;
+			</section>
+		</>
+	);
+};
+
+export default page;
