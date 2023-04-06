764ea40a0737cb56009a655703ac0a6200d8e4df
Jules Maestro <jlsodai@gmail.com>
2023-04-06 17:03:22 +0000

    change paperform application form

--- app/(form-pages)/esg-certificate-application/page.jsx
+++ app/(form-pages)/esg-certificate-application/page.jsx
@@ -6,7 +6,7 @@ const page = () => {
 		<>
 			<HeaderTitle title="ESG Certification Application." />
 			<section className="container my-10" data-aos="fade-up">
-				<PaperForm formSlug="esgcertificateapplicationform" />;
+				<PaperForm formSlug="hfgkw9hi" />;
 			</section>
 		</>
 	);
