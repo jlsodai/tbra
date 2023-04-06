3d297d71de7209a1447f042ce575982d5c4094d7
Jules Maestro <jlsodai@gmail.com>
2023-04-06 10:16:26 +0000

    create paperform component

---
+++ app/components/PaperForm.jsx
@@ -0,0 +1,13 @@
+"use client";
+import { useEffect } from "react";
+
+const PaperForm = ({ formSlug, showSpinner = 1 }) => {
+	useEffect(() => {
+		const script = document.createElement("script");
+		script.src = "https://paperform.co/__embed.min.js";
+		document.body.appendChild(script);
+	}, []);
+
+	return <div data-paperform-id={formSlug} data-spinner={showSpinner} />;
+};
+export default PaperForm;
