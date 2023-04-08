8ad04ffbf8a3e6ba722262727e41e3157ec64dac
Jules Maestro <jlsodai@gmail.com>
2023-04-08 12:46:34 +0000

    attempt domain wildcard redirect... works; commented out

--- next.config.js
+++ next.config.js
@@ -13,6 +13,15 @@ const nextConfig = {
 			},
 		],
 	},
+	// async redirects() { // redirects old files to new domain name
+	// 	return [
+	// 		{
+	// 			source: "/wp-content/:path*",
+	// 			destination: "https://theboardroomafrica.com/wp-content/:path*",
+	// 			permanent: false,
+	// 		},
+	// 	];
+	// },
 };
 
 module.exports = nextConfig;
