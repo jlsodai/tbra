2e38948a9dc8ec17179a90014eb7aed60c9bd881
Jules Maestro <jlsodai@gmail.com>
2023-04-08 15:44:03 +0000

    remove newsletter form borders

--- app/ui/FormInput.jsx
+++ app/ui/FormInput.jsx
@@ -3,7 +3,7 @@ const FormInput = ({ label, type = "text", required = false }) => {
 		<div className="md:flex md:items-center mt-4">
 			<div className="w-full">
 				<input
-					className="bg-white appearance-none border-2 border-dawn-200 w-full py-2 px-4 focus:outline-none focus:bg-white "
+					className="bg-white appearance-none w-full py-2 px-4 focus:outline-none focus:bg-white "
 					type={type}
 					placeholder={label}
 				/>
