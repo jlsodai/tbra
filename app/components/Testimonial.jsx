88e3200902428db6aab4a135eda09fc4fc940d7f
Jules Maestro <jlsodai@gmail.com>
2023-04-09 02:09:27 +0000

    fix responsiveness for home page testimonial

--- app/components/Testimonial.jsx
+++ app/components/Testimonial.jsx
@@ -1,7 +1,7 @@
 import { RiDoubleQuotesL } from "react-icons/ri";
 const Testimonial = ({ quote, author, authorTitle }) => {
 	return (
-		<div className="QuoteCascade after:bigborder pt-10">
+		<div className="bg-darkblue px-8 md:px-0 md:pr-24 pb-10 text-white md:QuoteCascade after:bigborder pt-10 -mx-8 md:mx-0">
 			<RiDoubleQuotesL className="text-dawn mx-auto text-[130px]" />
 			<p className="mt-4">{quote}</p>
 			<p className="mt-4">
