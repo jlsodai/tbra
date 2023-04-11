0773e1330d2b23519daac9d3cbc308abcd95dc53
Jules Maestro <jlsodai@gmail.com>
2023-04-11 13:30:58 +0000

    fix paperform x color; remove cancel at footer

--- app/components/PaperFormModal.jsx
+++ app/components/PaperFormModal.jsx
@@ -36,7 +36,7 @@ export default function PaperFormModal({ btnText, paperform }) {
 									<div className="bg-white px-4 pt-12">
 										<PaperForm formSlug={paperform} />
 									</div>
-									<div className="bg-gray-50 px-4 py-3 flex justify-center">
+									{/* <div className="bg-gray-50 px-4 py-3 flex justify-center">
 										<button
 											type="button"
 											className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
@@ -44,7 +44,7 @@ export default function PaperFormModal({ btnText, paperform }) {
 										>
 											Cancel
 										</button>
-									</div>
+									</div> */}
 								</div>
 							</div>
 						</div>
