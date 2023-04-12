cf174314fb2ee90a236a1afc1b31accb1a5fc757
Jules Maestro <jlsodai@gmail.com>
2023-04-12 08:14:04 +0000

    add membership application page

---
+++ app/membership/application/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const page = () => {
+	return (
+		<iframe
+			src="https://theboardroomafrica.com/application-form"
+			frameborder="0"
+			width="100%"
+			style={{
+				height: "100dvh",
+				width: "100%",
+				border: "none",
+				overflow: "hidden",
+			}}
+		></iframe>
+	);
+};
+
+export default page;
