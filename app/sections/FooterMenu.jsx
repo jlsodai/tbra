ff9d17e16dcffd6e5d809b23489800c8e177b850
Jules Maestro <jlsodai@gmail.com>
2023-04-13 12:53:21 +0000

    footer section initial padding removed

--- app/sections/FooterMenu.jsx
+++ app/sections/FooterMenu.jsx
@@ -6,7 +6,7 @@ import { bottomMenu } from "../utils/options";
 
 const FooterMenu = () => {
 	return (
-		<footer className="px-4 bg-tender text-dawn-200 bigborder">
+		<footer className="bg-tender text-dawn-200 bigborder">
 			<div className="container flex flex-col justify-between py-28 mx-auto space-y-8 lg:flex-row lg:space-y-0">
 				<div className="lg:w-1/5">
 					<a
