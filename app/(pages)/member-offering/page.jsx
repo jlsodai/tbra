2fbf61bd0bbff2664ae2340c79d22328b0648774
Jules Maestro <jlsodai@gmail.com>
2023-06-06 09:13:58 +0000

    add paperform to member offering page

--- app/(pages)/member-offering/page.jsx
+++ app/(pages)/member-offering/page.jsx
@@ -1,16 +1,42 @@
 import HeaderTitle from "@/app/sections/HeaderTitle";
 import OfferSection from "@/app/sections/OfferSection";
 import { offers, newoffers } from "@/app/utils/options";
+import PaperFormModal from "@/app/components/PaperFormModal";
 
 const page = () => {
 	return (
 		<>
-			<HeaderTitle title="Membership Offerings" />
-			<OfferSection bg="tender" offers={newoffers.access} />
-			<OfferSection bg="mustard" offers={newoffers.network} />
-			<OfferSection bg="tender" offers={newoffers.partner} />
-			<OfferSection offers={newoffers.presence} />
-			<OfferSection bg="mustard" offers={newoffers.thoughtLeadership} />
+			<HeaderTitle
+				title="Membership Offerings"
+				paperForm={
+					<PaperFormModal
+						btnText="Get Started"
+						paperform="memberoffers"
+						className="btn mt-4 rounded font-normal bg-tender text-white"
+					/>
+				}
+			/>
+			<div className="mt-16">
+				<OfferSection
+					bg="tender"
+					offers={newoffers.access}
+					accordionOpen={true}
+				/>
+				<OfferSection bg="mustard" offers={newoffers.network} />
+				<OfferSection bg="tender" offers={newoffers.partner} />
+				<OfferSection offers={newoffers.presence} />
+				<OfferSection
+					bg="mustard"
+					offers={newoffers.thoughtLeadership}
+				/>
+			</div>
+			<section className="container mt-8 text-center">
+				<PaperFormModal
+					btnText="Get Started"
+					paperform="memberoffers"
+					// className="btn mt-4 rounded text-white"
+				/>
+			</section>
 		</>
 	);
 };
