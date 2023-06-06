db1c66cd0de9470a9fe4871b31ede4bd816dba96
Jules Maestro <jlsodai@gmail.com>
2023-06-06 09:13:30 +0000

    add paperform possibility

--- app/sections/HeaderTitle.jsx
+++ app/sections/HeaderTitle.jsx
@@ -1,14 +1,18 @@
 /* eslint-disable @next/next/no-img-element */
-const HeaderTitle = ({ title }) => {
+import PaperFormModal from "@/app/components/PaperFormModal";
+const HeaderTitle = ({ title, paperForm }) => {
 	return (
-		<section className="bg-darkblue bigborder" data-aos="fade-up">
+		<section className="bg-darkblue bigborder">
 			<div className="flex container min-h-[280px] md:min-h-[380px] relative justify-center items-center">
 				<img
 					src="/images/white-key.png"
 					className="absolute left-0 h-[75%] -z-1 opacity-5"
 					alt=""
 				/>
-				<h1 className="md:max-w-[80%] py-16">{title}</h1>
+				<div className="md:max-w-[80%] py-16">
+					<h1>{title}</h1>
+					{paperForm}
+				</div>
 			</div>
 		</section>
 	);
