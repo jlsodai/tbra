e25fc04d9a144c813c5482daff6cccd4a5eb767f
Jules Maestro <jlsodai@gmail.com>
2023-07-29 06:04:30 +0000

    disable fetchData.js

--- app/lib/fetchData.js
+++ app/lib/fetchData.js
@@ -1,10 +1,11 @@
 export async function fetchData(endpoint) {
-	const res = await fetch(
-		`${process.env.NEXT_PUBLIC_STRAPI_URL}/${endpoint}`,
-		{ cache: "no-store" }
-	);
-	if (!res.ok) {
-		throw new Error("Failed to fetch data");
-	}
-	return res.json();
+	// const res = await fetch(
+	// 	`${process.env.NEXT_PUBLIC_STRAPI_URL}/${endpoint}`,
+	// 	{ cache: "no-store" }
+	// );
+	// if (!res.ok) {
+	// 	throw new Error("Failed to fetch data");
+	// }
+	// return res.json();
+	return true;
 }
