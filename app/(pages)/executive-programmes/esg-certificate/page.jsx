a405b448ef9ed2bb04f1d4b7efa81c8b049f9c61
Jules Maestro <jlsodai@gmail.com>
2023-07-29 00:02:56 +0000

    remove cpd from esg

--- app/(pages)/executive-programmes/esg-certificate/page.jsx
+++ app/(pages)/executive-programmes/esg-certificate/page.jsx
@@ -8,37 +8,37 @@ import ProgrammeLogos from "@/app/components/ProgrammeLogos";
 import { filteredMeta } from "@/app/utils/options";
 
 const pageData = async () => {
-	return await fetchData("programmes/3?populate=*");
+  return await fetchData("programmes/3?populate=*");
 };
 
 export async function generateMetadata() {
-	const data = await pageData();
-	const { seo } = data?.data?.attributes;
-	return filteredMeta(seo);
+  const data = await pageData();
+  const { seo } = data?.data?.attributes;
+  return filteredMeta(seo);
 }
 
 export default async function Page() {
-	const data = await pageData();
-	const attributes = data?.data?.attributes;
+  const data = await pageData();
+  const attributes = data?.data?.attributes;
 
-	return (
-		<>
-			<BrochureDrawer brochureDetails={attributes.brochureInfo} />
-			<HeaderTitle title="ESG Certificate" />
+  return (
+    <>
+      <BrochureDrawer brochureDetails={attributes.brochureInfo} />
+      <HeaderTitle title="ESG Certificate" />
 
-			<ProgramBlocks attributes={attributes} certified={true} />
+      <ProgramBlocks attributes={attributes} />
 
-			<section className="bg-white" data-aos="fade-up">
-				<div className="container my-28">
-					<h2 className="text-center text-dusk">
-						Some of the companies that trust our instructor&apos;s
-						experience include:
-					</h2>
-					<ProgrammeLogos logos={attributes.trustedBy} />
-				</div>
-			</section>
+      <section className="bg-white" data-aos="fade-up">
+        <div className="container my-28">
+          <h2 className="text-center text-dusk">
+            Some of the companies that trust our instructor&apos;s experience
+            include:
+          </h2>
+          <ProgrammeLogos logos={attributes.trustedBy} />
+        </div>
+      </section>
 
-			<StatsSection />
-		</>
-	);
+      <StatsSection />
+    </>
+  );
 }
