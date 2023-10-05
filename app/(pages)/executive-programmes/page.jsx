1a810fd315b6b8fcb8f1bb6497788f59c6f9a239
Jules Maestro <jlsodai@gmail.com>
2023-10-05 11:54:46 +0000

    update executive programmes cpd section

--- app/(pages)/executive-programmes/page.jsx
+++ app/(pages)/executive-programmes/page.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import HeaderTitle from "@/app/sections/HeaderTitle";
 import CascadeImg from "@/app/components/CascadeImg";
 import KeySection from "@/app/sections/KeySection";
@@ -27,8 +28,8 @@ const Page = async () => {
       <HeaderTitle title="Explore powerful executive programmes as you expand your influence at the table." />
 
       <section className="container" data-aos="fade-up">
-        <div className="keySectionWrap">
-          <KeySection heading={intro.Title}>
+        <div className="keySectionWrap py-16">
+          <KeySection heading={intro.Title} className="!py-0">
             <p>{intro.content}</p>
           </KeySection>
           {/* <CascadeImg
@@ -38,6 +39,19 @@ const Page = async () => {
 						imgClass="object-right"
 						src="https://tbratest.my.canva.site/board-roles-individual-page/images/40a1c0cfce04c8c78e2edc7741812803.jpg"
 					/> */}
+          <div className="md:flex item-center items-center justify-between gap-8 bg-dawn-200 px-8 py-6 mt-6">
+            <img
+              className="sm:m-0 -sm:mb-4 -sm:mt-0  -sm:mx-auto"
+              src="https://res.cloudinary.com/tbra/image/upload/v1690439782/cpdCertified_ysk3a2.png"
+              alt="CPD Certified"
+              width="130px"
+            />
+            <p className="m-0 leading-normal text-sm">
+              This programme is certified by The CPD Certification Service. It
+              may be applicable to individuals who are members of, or are
+              associated with, UK-based professional bodies.
+            </p>
+          </div>
         </div>
       </section>
 
