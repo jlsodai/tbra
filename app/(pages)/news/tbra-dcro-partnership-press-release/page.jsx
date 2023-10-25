cb34a979654f9c68cec8bd3565fa2197f3383717
Jules Maestro <jlsodai@gmail.com>
2023-10-25 13:53:57 +0000

    add linebreak to title

--- app/(pages)/news/tbra-dcro-partnership-press-release/page.jsx
+++ app/(pages)/news/tbra-dcro-partnership-press-release/page.jsx
@@ -10,7 +10,7 @@ const page = () => {
 
 				<div className="prose max-w-full lg:mx-44">
 					<div className="flex items-center">
-						<h1>Empowering Women and Elevating Risk Governance: The DCRO Institute and TheBoardroom Africa Join Forces</h1>
+						<h1>Empowering Women and Elevating Risk Governance: <br/>The DCRO Institute and TheBoardroom Africa Join Forces</h1>
 						<img className="mt-0 ml-16 self-baseline" src="https://res.cloudinary.com/tbra/image/upload/v1698241406/dcro_slumhm.jpg" width="200px" />
 					</div>
 					<Markdown>{pressrelease}</Markdown>
