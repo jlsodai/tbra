a4f2bfb119e835a7056acaf7d6b58de163288496
Julius A. Odai <jlsodai@gmail.com>
2024-03-11 09:57:14 +0000

    add aos for member services

--- app/member-services/page.jsx
+++ app/member-services/page.jsx
@@ -27,15 +27,23 @@ const page = async () => {
       <section className="bg-[linear-gradient(rgba(19,30,61,0.75),rgba(19,30,61,0.75)),url('https://res.cloudinary.com/dhhw72iwq/image/upload/v1708510120/member-services-herobg_dpdh7z.jpg')] bg-cover bg-[-400px] md:bg-top items-center min-h-screen flex relative">
         <div className="container grid md:grid-cols-2">
           <div className="py-8">
-            <h1 className="leading-tight text-4xl md:text-6xl">
+            <h1
+              className="!leading-tight text-4xl md:text-6xl"
+              data-aos="fade-up"
+              data-aos-delay="150"
+            >
               Membership to support your leadership journey
             </h1>
-            <p className="mt-8 md:text-2xl text-[#EEEAD6]">
+            <p
+              className="mt-8 md:text-2xl text-[#EEEAD6]"
+              data-aos="fade-up"
+              data-aos-delay="350"
+            >
               From personal branding and executive presence to thought
               leadership positioning, we&apos;ve curated an experience to
               elevate every facet of your professional journey.
             </p>
-            <div className="mt-8">
+            <div className="mt-8" data-aos="fade-up" data-aos-delay="550">
               <Link
                 href="#"
                 className="btn bg-mustard md:text-xl uppercase text-darkblue font-medium"
@@ -48,7 +56,7 @@ const page = async () => {
             <img
               src="/images/white-key.png"
               data-aos="flip-right"
-              data-aos-delay="450"
+              data-aos-delay="950"
               data-aos-once="false"
               className="md:block w-[60px] md:w-[100px] absolute bottom-14"
               alt=""
@@ -58,24 +66,30 @@ const page = async () => {
       </section>
 
       <section className="bg-[#DADEDA] flex items-center">
-        <div className="min-h-screen container grid md:grid-cols-2 gap-x-16 items-center">
+        <div className="container grid md:grid-cols-2 gap-x-16 items-center">
           <div className="self-end items-end justify-end flex md:order-last">
             <img
+              data-aos="fade-up"
+              data-aos-delay="850"
               src="https://res.cloudinary.com/dhhw72iwq/image/upload/v1708961780/linkedInMakeover_eoq9jr.jpg"
-              className="-md:object-cover -md:h-60 -md:w-full md:w-[75%]"
+              className="-md:object-cover -md:h-60 -md:w-full md:w-[75%] md:mt-32"
               alt=""
             />
           </div>
           <div className="py-8 -md:mb-16">
-            <h1 className="leading-tight text-2xl md:text-5xl text-darkblue">
+            <h1
+              className="!leading-tight text-2xl md:text-5xl text-darkblue"
+              data-aos="fade-up"
+              data-aos-delay="150"
+            >
               Get a complete <br /> LinkedIn Makeover
             </h1>
-            <p className="mt-8">
+            <p className="mt-8" data-aos="fade-up" data-aos-delay="350">
               Have your LinkedIn profile evaluated by our professional coaches
               who will provide customised advice to boost your visibility. They
               are also ready to apply those improvements on your behalf.
             </p>
-            <div className="mt-8">
+            <div className="mt-8" data-aos="fade-up" data-aos-delay="550">
               <Link
                 target="_blank"
                 href="https://tbramemberoffers.paperform.co/?sectionDisplay=linkedin"
@@ -90,7 +104,7 @@ const page = async () => {
 
       <section className="bg-[#F4F1EC] flex">
         <div className="md:container grid md:grid-cols-2 gap-x-16">
-          <div className=" bg-white">
+          <div className=" bg-white" data-aos="fade-up" data-aos-delay="150">
             <img
               src="https://res.cloudinary.com/dhhw72iwq/image/upload/w_800,h_434,c_fill,g_north/v1708962811/tell-story_fafqbn.jpg"
               alt=""
@@ -103,16 +117,20 @@ const page = async () => {
             </div>
           </div>
           <div className="px-8 py-16 md:p-16 flex flex-col justify-center">
-            <h2 className="leading-tight text-2xl md:text-5xl text-darkblue">
+            <h2
+              className="!leading-tight text-2xl md:text-5xl text-darkblue"
+              data-aos="fade-up"
+              data-aos-delay="150"
+            >
               Establish your thought leadership presence.
             </h2>
-            <p className="mt-8">
+            <p className="mt-8" data-aos="fade-up" data-aos-delay="350">
               With the backing of a global network of researchers, our
               specialists will transform your distinct viewpoints into engaging
               opinion articles and LinkedIn posts that position you as a top
               voice, and leading authority in your field.
             </p>
-            <div className="mt-8">
+            <div className="mt-8" data-aos="fade-up" data-aos-delay="550">
               <Link
                 target="_blank"
                 href="https://tbramemberoffers.paperform.co/?sectionDisplay=thought"
@@ -128,16 +146,24 @@ const page = async () => {
       <section className="bg-[url('https://res.cloudinary.com/dhhw72iwq/image/upload/v1708963279/more-with-prime-bulbs_vxdjmc.jpg')] bg-cover flex items-center">
         <div className="container grid md:grid-cols-2 gap-x-16">
           <div className="py-24">
-            <h1 className="leading-tight text-2xl md:text-5xl">
+            <h1
+              className="!leading-tight text-2xl md:text-5xl"
+              data-aos="fade-up"
+              data-aos-delay="150"
+            >
               Do more with Prime
             </h1>
-            <p className="mt-8 text-white">
+            <p
+              className="mt-8 text-white"
+              data-aos="fade-up"
+              data-aos-delay="350"
+            >
               TBrA Prime is an enhanced premium membership experience, crafted
               to enable you to get the absolute best from your membership. Enjoy
               6 complimentary group coaching sessions, priority access to member
               services, exclusive discounts, and more.
             </p>
-            <div className="mt-8">
+            <div className="mt-8" data-aos="fade-up" data-aos-delay="550">
               <Link
                 href="https://theboardroomafrica.com/tbraprime"
                 className="btn bg-transparent hover:bg-white hover:text-dusk border px-16 text-white font-bold"
@@ -152,24 +178,34 @@ const page = async () => {
         </div>
       </section>
 
-      <section className="min-h-screen bg-[#DADEDA] flex items-center">
+      <section className="bg-[#DADEDA] flex items-center">
         <div className="container grid md:grid-cols-2 gap-x-16 items-center">
           <div className="py-8">
             <img
+              data-aos="fade-up"
+              data-aos-delay="150"
               src="https://res.cloudinary.com/dhhw72iwq/image/upload/w_500,h_500,g_east,c_fill/v1709022763/woman-sitting_p1vxfb.jpg"
               className="md:hidden -md:my-8"
               alt=""
             />
-            <h1 className="leading-tight text-2xl md:text-5xl text-darkblue">
+            <h1
+              className="!leading-tight text-2xl md:text-5xl text-darkblue"
+              data-aos="fade-up"
+              data-aos-delay="250"
+            >
               Prepare for your next board or executive interview
             </h1>
-            <p className="mt-8">
+            <p className="mt-8" data-aos="fade-up" data-aos-delay="350">
               Collaborate with skilled professionals to strategically
               differentiate yourself for prestigious board positions and
               executive roles.
             </p>
             <div className="mt-8 flex flex-col gap-y-3">
-              <p className="flex items-center">
+              <p
+                className="flex items-center"
+                data-aos="fade-up"
+                data-aos-delay="150"
+              >
                 <img
                   src="https://res.cloudinary.com/dhhw72iwq/image/upload/co_rgb:946F38,e_colorize:100/v1705057028/bulb_xzxuqw.png"
                   className="mr-4"
@@ -178,7 +214,11 @@ const page = async () => {
                 />
                 Discover your strengths, weaknesses, and unique selling points
               </p>
-              <p className="flex items-center">
+              <p
+                className="flex items-center"
+                data-aos="fade-up"
+                data-aos-delay="350"
+              >
                 <img
                   src="https://res.cloudinary.com/dhhw72iwq/image/upload/co_rgb:946F38,e_colorize:100/v1705057028/bulb_xzxuqw.png"
                   className="mr-4"
@@ -187,7 +227,11 @@ const page = async () => {
                 />
                 Master strategies to tackle diverse interview questions
               </p>
-              <p className="flex items-center">
+              <p
+                className="flex items-center"
+                data-aos="fade-up"
+                data-aos-delay="550"
+              >
                 <img
                   src="https://res.cloudinary.com/dhhw72iwq/image/upload/co_rgb:946F38,e_colorize:100/v1705057028/bulb_xzxuqw.png"
                   className="mr-4"
@@ -199,7 +243,11 @@ const page = async () => {
               </p>
             </div>
           </div>
-          <div className="md:flex flex-col justify-center items-center">
+          <div
+            className="md:flex flex-col justify-center items-center md:my-32"
+            data-aos="fade-up"
+            data-aos-delay="150"
+          >
             <img
               src="https://res.cloudinary.com/dhhw72iwq/image/upload/w_500,h_500,g_east,c_fill/v1709022763/woman-sitting_p1vxfb.jpg"
               className="-md:hidden"
@@ -209,6 +257,8 @@ const page = async () => {
               target="_blank"
               href="https://tbramemberoffers.paperform.co/?sectionDisplay=thought"
               className="btn bg-mustard px-16 text-darkblue font-bold -md:mb-16 md:mt-8"
+              data-aos="fade-up"
+              data-aos-delay="350"
             >
               SIGN UP
             </Link>
@@ -216,9 +266,13 @@ const page = async () => {
         </div>
       </section>
 
-      <section className="bg-[#F4F1EC] md:min-h-screen flex flex-col justify-end">
+      <section className="bg-[#F4F1EC] flex flex-col justify-end">
         <div className="container mt-16 md:mt-24">
-          <h1 className="leading-tight text-2xl md:text-5xl text-darkblue max-w-xl">
+          <h1
+            className="!leading-tight text-2xl md:text-5xl text-darkblue max-w-xl md:mt-32"
+            data-aos="fade-up"
+            data-aos-delay="150"
+          >
             Unlock Your Potential with Our Tailored Executive CV Services
           </h1>
         </div>
@@ -228,11 +282,17 @@ const page = async () => {
               target="_blank"
               href="https://tbramemberoffers.paperform.co/?sectionDisplay=cv"
               className="btn bg-transparent border px-16 border-darkblue text-darkblue font-bold"
+              data-aos="fade-up"
+              data-aos-delay="350"
             >
               SIGN UP
             </Link>
           </div>
-          <div className="text-darkblue bg-[#DADEDA] p-12 md:p-20 h-full">
+          <div
+            className="text-darkblue bg-[#DADEDA] p-12 md:p-20 h-full"
+            data-aos="fade-up"
+            data-aos-delay="350"
+          >
             <h2 className="text-darkblue">Executive CV Consultation</h2>
             <p className="mt-8">
               Stand out when applying for executive roles with a professionally
@@ -240,7 +300,11 @@ const page = async () => {
               guides you through areas for improvement.
             </p>
           </div>
-          <div className="text-darkblue bg-[#C3CBC3] p-12 md:p-20 h-full">
+          <div
+            className="text-darkblue bg-[#C3CBC3] p-12 md:p-20 h-full"
+            data-aos="fade-up"
+            data-aos-delay="550"
+          >
             <h2 className="text-darkblue">Executive CV Makeover</h2>
             <p className="mt-8">
               Elevate your profile further with an intensive 3-hour makeover led
@@ -251,18 +315,28 @@ const page = async () => {
         </div>
       </section>
 
-      <section className="bg-[linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)),url('https://res.cloudinary.com/dhhw72iwq/image/upload/v1708521754/member-services-gcbg_aucprn.jpg')] bg-cover bg-top items-center min-h-screen flex">
-        <div className="container">
-          <span className="bg-[#4A4F54]/60 text-white px-16 py-3">
+      <section className="bg-[linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)),url('https://res.cloudinary.com/dhhw72iwq/image/upload/v1708521754/member-services-gcbg_aucprn.jpg')] bg-cover bg-top items-end h-screen max-h-[900px] flex">
+        <div className="container md:mb-32">
+          <span
+            className="bg-[#4A4F54]/60 text-white px-16 py-3"
+            data-aos="fade-up"
+            data-aos-delay="150"
+          >
             GROUP COACHING
           </span>
-          <p className="text-white text-6xl max-w-3xl mt-8">
+          <p
+            className="text-white text-6xl max-w-3xl mt-8"
+            data-aos="fade-up"
+            data-aos-delay="350"
+          >
             Embark on a journey of growth and self-discovery
           </p>
           <Link
             target="_blank"
             href="https://tbramemberoffers.paperform.co/?sectionDisplay=gc"
             className="btn bg-transparent hover:bg-white hover:text-dusk border px-16 mt-8 text-white font-bold"
+            data-aos="fade-up"
+            data-aos-delay="550"
           >
             Learn more
           </Link>
@@ -277,17 +351,30 @@ const page = async () => {
             alt=""
           />
           <div className="text-mustard md:max-w-[60%] z-10 text-center">
-            <h4 className="text-3xl md:text-6xl font-libreb">
+            <h4
+              className="text-3xl md:text-6xl font-libreb"
+              data-aos="fade-up"
+              data-aos-delay="150"
+            >
               Join the movement
             </h4>
-            <p className="mt-4 font-halyard font-extralight text-2xl md:text-3xl text-mustard">
+            <p
+              className="mt-4 font-halyard font-extralight text-2xl md:text-3xl text-mustard"
+              data-aos="fade-up"
+              data-aos-delay="350"
+            >
               Discover the power of doing it together.
             </p>
           </div>
 
           <div className="grid md:grid-cols-3 gap-10 text-white md:max-w-[80%]">
             {testimonials.map((testimonial, i) => (
-              <div key={i} className="border border-white p-6">
+              <div
+                key={i}
+                className="border border-white p-6"
+                data-aos="fade-up"
+                data-aos-delay={350 * i}
+              >
                 <p>{testimonial.quote}</p>
                 <p className="mt-8">- {testimonial.author}</p>
                 <p>{testimonial.title}</p>
@@ -299,6 +386,8 @@ const page = async () => {
             <Link
               href="/membership/application"
               className="border-2 btn border-mustard"
+              data-aos="fade-up"
+              data-aos-delay="550"
             >
               Apply
             </Link>
