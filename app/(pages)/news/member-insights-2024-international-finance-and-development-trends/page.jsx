432d4b429a3b5254c2a01b5557f796f0a762b397
Julius A. Odai <jlsodai@gmail.com>
2024-03-26 10:52:42 +0000

    add member insight

---
+++ app/(pages)/news/member-insights-2024-international-finance-and-development-trends/page.jsx
@@ -0,0 +1,154 @@
+/* eslint-disable @next/next/no-img-element */
+import Markdown from "markdown-to-jsx";
+import { pressrelease } from "@/app/utils/pressrelease";
+import HeaderTitle from "@/app/sections/HeaderTitle";
+
+export const metadata = {
+  title: "Member Insights: 2024 International Finance & Development Trends",
+  // openGraph: {
+  // 	images: 'https://res.cloudinary.com/tbra/image/upload/v1698250891/TBRA-DCRO-Press-Release_bvmzjo.jpg'
+  // },
+};
+
+const page = () => {
+  return (
+    <>
+      <section className="bg-darkblue bigborder">
+        <div className="flex container min-h-[280px] md:min-h-[380px] relative justify-center items-center">
+          <img
+            src="/images/white-key.png"
+            className="absolute left-0 h-[75%] -z-1 opacity-5"
+            alt=""
+          />
+          <div className="md:max-w-[90%] py-16">
+            <h1 className="text-4xl md:text-6xl">
+              Member Insights: <br />
+              <span className="text-white">
+                2024 International Finance & Development Trends
+              </span>
+            </h1>
+          </div>
+        </div>
+      </section>
+      <section className="container mt-16">
+        <div className="prose max-w-full lg:mx-16">
+          <img
+            src="https://res.cloudinary.com/dhhw72iwq/image/upload/v1711449462/malado_kxil5y.jpg"
+            className="float-left mr-6 mb-6 w-60"
+            alt=""
+          />
+          <h2>
+            &quot;Macroeconomic policies are central to pursuing gender equality
+            and accelerating investment in women, particularly in Africa.&quot;
+          </h2>
+          <p>
+            Macroeconomic policies working for women and climate change:
+            Macroeconomic policies are not neutral to gender and are central to
+            pursuing gender equality and accelerating investment in women,
+            particularly in Africa.
+          </p>
+          <p>
+            At Falémé Conseil, we have been working to ensure these policies
+            (fiscal, monetary, public investments) are increasingly
+            gender-responsive and we believe this centrality will continue to
+            gain traction. Finance ministries and Central banks are ideally
+            placed to impact a wide range of areas that can significantly
+            improve the lives of women and ensure that the risk perception is
+            reduced to accelerate their access to funding.
+          </p>
+          <p>
+            But these players are also key to continue pushing for a global
+            financial architecture that works for Africa, its women and climate
+            change. We have been working, with others, to have these
+            stakeholders increasingly looking at how economic and financial
+            policies can be used to mitigate the impact of climate change,
+            without jeopardising the development goals. Despite the challenges,
+            we strongly believe these trends will continue gaining
+          </p>
+          <h2>
+            The volatile and uncertain environment created by the repeal of
+            constitutions, laws, and other regulatory frameworks affecting
+            businesses requires nimble and solid boardrooms.
+          </h2>
+          <p>
+            Boardrooms in &quot;transition&quot; countries: a silver lining for
+            directors?: Transition countries (as they are referred to by
+            development institutions, countries that undergo a political
+            transition with sometimes far-reaching socio-economic consequences)
+            challenge the boardroom.
+          </p>
+          <p>
+            The volatile and uncertain environment created by the repeal of
+            constitutions, laws, and other regulatory frameworks affecting
+            businesses requires nimble and solid boardrooms. It requires that
+            directors stay focused to address strategy, governance, and risks
+            and ensure that value is created for shareholders. We have observed
+            first-hand that navigating these contexts demands at least three key
+            traits.
+          </p>
+          <p>
+            First, agility to adapt to sometimes &quot;erratic&quot;
+            decision-making and building relationships with new stakeholders is
+            of the essence. Second, staying ahead of the curve, while being able
+            to identify opportunities matters even more in these peculiar times
+            of unstable legal environment. Third, soft skills and a strengthened
+            support to executive teams are critical to navigate what may
+            sometimes be described as irrational situations. In 2024, these will
+            remain important tools for board directors exercising in these
+            complex settings.
+          </p>
+          <h2>
+            &quot;Collecting, analysing, and releasing more and quality data
+            will remain seminal, while leveraging the digital transformation and
+            the possibilities offered by artificial intelligence (AI)&quot;
+          </h2>
+          <p>
+            Data economy: what we know is what we measure and can ultimately
+            improve: Increasing evidence-based policymaking to deliver better
+            public services in support of our economies and improve the lives of
+            the populations matters.
+          </p>
+          <p>
+            This is crucial to avoid leading in the dark, and to increase both
+            accountability and transparency. Collecting, analysing, and
+            releasing more and quality data will remain seminal, while
+            leveraging the digital transformation and the possibilities offered
+            by artificial intelligence (AI).
+          </p>
+          <p>
+            We have seen (and been part of) how the landscape of the data
+            economy is gradually changing in Africa, with an increasing number
+            of African-led/based firms. This is a positive trend that should
+            enable more home-grown and context-specific solutions to overcome
+            the continent&apos;s challenges and ultimately shape an African
+            narrative in the domain of economics and finance.
+          </p>
+          <p>
+            The data economy can also contribute to enhancing Africa&apos;s
+            agency on the global stage. Further to this, we think that having
+            more women in that space will support more inclusive and innovative
+            proposals for a sweeping transformation of a public sector at the
+            service of Africa&apos;s populations and its economies.
+          </p>
+        </div>
+        <div className="max-w-2xl lg:mx-16">
+          <hr className="my-12 max-w-md h-1 bg-dawn-500" />
+          <p className="font-bold mb-4">About the author</p>
+          <p className="text-tender text-4xl font-bold">MALADO KABA</p>
+          <p className="font-bold mb-2">Managing Director of Falémé Conseil</p>
+          <p>
+            Malado Kaba is a development economist and professional in
+            international development and finance. Malado Kaba is the managing
+            director of Falémé Conseil and former Economy and Finance minister
+            of the Republic of Guinea. Until recently, she sat as the chairwoman
+            of Orabank Guinea and the Chair of the board of the Authority
+            regulating electricity and water in Guinea. She is a Trustee at
+            International Budget Parnership.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default page;
