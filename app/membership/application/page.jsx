4a926a772458f2abeb92cd789b64ce2158d9d1b5
Julius A. Odai <jlsodai@gmail.com>
2024-04-09 12:37:21 +0000

    update application page

--- app/membership/application/page.jsx
+++ app/membership/application/page.jsx
@@ -5,10 +5,7 @@ import HeaderTitle from "@/app/sections/HeaderTitle";
 const page = () => {
   return (
     <>
-      {/* <HeaderTitle title="Upgrade to TBrA Prime" /> */}
-      <section className="container my-10" data-aos="fade-up">
-        <PaperForm formSlug="membershipapplication" />
-      </section>
+      <PaperForm formSlug="membershipapplication" />
     </>
   );
 };
