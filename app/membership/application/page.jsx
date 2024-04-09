6ae958b2c8db1d10dc4c5ed254bf575b6d05b0cb
Julius A. Odai <jlsodai@gmail.com>
2024-04-09 12:04:04 +0000

    update website

--- app/membership/application/page.jsx
+++ app/membership/application/page.jsx
@@ -3,14 +3,14 @@ import PaperForm from "@/app/components/PaperForm";
 import HeaderTitle from "@/app/sections/HeaderTitle";
 
 const page = () => {
-	return (
-		<>
-			{/* <HeaderTitle title="Upgrade to TBrA Prime" /> */}
-			<section className="container my-10" data-aos="fade-up">
-				<PaperForm formSlug="tbra-membership-application" />
-			</section>
-		</>
-	);
+  return (
+    <>
+      {/* <HeaderTitle title="Upgrade to TBrA Prime" /> */}
+      <section className="container my-10" data-aos="fade-up">
+        <PaperForm formSlug="membershipapplication" />
+      </section>
+    </>
+  );
 };
 
 export default page;
