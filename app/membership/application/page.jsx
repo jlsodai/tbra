8fd4d1ce44ec902ddddd9d3d3244e7c6e8bbddf4
Julius A. Odai <jlsodai@gmail.com>
2024-05-14 14:13:20 +0000

    update new membership application

--- app/membership/application/page.jsx
+++ app/membership/application/page.jsx
@@ -6,12 +6,12 @@ const page = () => {
   return (
     <>
       {/* <HeaderTitle title="Upgrade to TBrA Prime" /> */}
-      <section className="container my-10" data-aos="fade-up">
+      {/* <section className="container my-10" data-aos="fade-up">
         <PaperForm formSlug="tbra-membership-application" />
-      </section>
-      {/* <section className="bg-[#202945] py-16">
-        <PaperForm formSlug="membershipapplication" />
       </section> */}
+      <section className="bg-[#202945] py-16">
+        <PaperForm formSlug="membershipapplication" />
+      </section>
     </>
   );
 };
