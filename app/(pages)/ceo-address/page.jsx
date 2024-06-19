3a420e5d66c6532f7cde19a901ab5ce53bc8c67c
Julius A. Odai <jlsodai@gmail.com>
2024-06-19 13:10:37 +0000

    add ceo address

---
+++ app/(pages)/ceo-address/page.jsx
@@ -0,0 +1,136 @@
+/* eslint-disable @next/next/no-img-element */
+import HeaderTitle from "@/app/sections/HeaderTitle";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <HeaderTitle title="CEO Address" />
+      <section className="container mt-16">
+        <div className="prose max-w-full lg:mx-16">
+          <h1 className="max-w-4xl">
+            Strengthening our Mission by Ensuring Men Are Part of the Solution
+          </h1>
+
+          <p className="mb-8 mt-4">
+            <b>June 2024</b>
+          </p>
+
+          <img
+            src="https://res.cloudinary.com/tbra/image/upload/v1680706201/marcia_yzavpl.jpg"
+            width="200px"
+            className="float-left mr-8 mb-8 mt-0"
+            alt=""
+          />
+
+          <p>To all our stakeholders,</p>
+
+          <p>
+            Since its inception, TheBoardroom Africa (TBrA) has been dedicated
+            to advocating for women&apos;s representation on boards and other
+            leadership positions, driven by the stark disparity between men and
+            women at the top. Over the past eight years, we have been unwavering
+            in our advocacy for the inclusion of women in key leadership
+            positions.
+          </p>
+
+          <p>
+            By remaining adaptable, we have developed a sustainable model that
+            aligns with our mission. This commitment has led to a transformative
+            agenda, directly impacting the leadership journeys of thousands of
+            women. We have collaborated with regional and global businesses and
+            organisations to reshape their leadership spaces by placing over 200
+            women executives on boards, investment committees, and other
+            leadership roles.
+          </p>
+
+          <p>
+            Designed to help leaders navigate the complex challenges of
+            today&apos;s rapidly changing world, our executive certificate
+            programmes, developed by leaders for leaders, with contextual and
+            practical solutions, have equipped more than 600 women leaders with
+            the knowledge and skills necessary to transform their approaches to
+            governance and sustainability. We&apos;ve done this all while also
+            providing our members with a vibrant community of fellow leaders
+            with whom they can support and collaborate.
+          </p>
+
+          <div className="bg-[#DADFE5] mb-8 ml-8 text-black px-8 float-right max-w-[400px] font-libreb text-2xl">
+            <img
+              src="https://res.cloudinary.com/dqngxqaha/image/upload/v1704894801/quote_puq3qe.png"
+              width="50px"
+              className="mt-4 mb-0"
+              alt=""
+            />
+            <p className="mt-2">
+              We believe the inclusion of men in our community will make our
+              impact more transformational. We also see this as a way to engage
+              men broadly around conversations of inclusion and diversity - and
+              truly make them part of the solution.
+            </p>
+          </div>
+
+          <p>
+            Today, I am writing to share an important and exciting development
+            in our ongoing mission to foster diversity within the corporate
+            landscape. In our efforts to find the best, most effective ways to
+            elevate the status of women leaders, we are continuously listening,
+            learning and unlearning to stay relevant to our mission.
+          </p>
+
+          <p>
+            We have come to the realisation that in order to make leadership
+            teams diverse, we need to be equally intentional and ensure that all
+            leaders, regardless of gender, are provided with the right tools and
+            that our mission to enhance leadership opportunities for women is
+            made richer in discourse and networks and communities that are also
+            diverse. We recognise that for women to advance in leadership, they
+            need to be part of a supportive community and expand their access to
+            men who hold the majority of leadership positions.
+          </p>
+
+          <p>
+            To that effect, we are excited to announce that TheBoardroom Africa
+            will be opening its membership to male candidates. We believe the
+            inclusion of men in our community will make our impact more
+            transformational, and we also see this as a way to engage men
+            broadly around conversations of inclusion and diversity - and truly
+            make them part of the solution.
+          </p>
+
+          <p>
+            We understand that change can bring about questions. We have
+            prepared this{" "}
+            <a href="https://theboardroomafrica.com/malemembershipfaqs">
+              <b>Q&A</b>
+            </a>{" "}
+            to answer some of them. If there are questions we have not
+            addressed, you may email them to:{" "}
+            <a href="mailto:info@theboardroomafrica.com">
+              <b>info@theboardroomafrica.com</b>
+            </a>
+          </p>
+
+          <p>
+            Thank you for your continued commitment to TheBoardroom Africa. Your
+            support and dedication have been instrumental in our success so far.
+            We look forward to continuing this journey together, breaking
+            barriers and paving the way for a future where authentic diversity
+            is ingrained in the fabric of executive leadership.
+          </p>
+
+          <p>Warm regards</p>
+          <img
+            src="https://res.cloudinary.com/dqngxqaha/image/upload/v1704895148/marciaSign_lzc0mt.png"
+            width="250px"
+            alt=""
+          />
+          <p>Founder & CEO</p>
+          <p>TheBoardroom Africa</p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default page;
