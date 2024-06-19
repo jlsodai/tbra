79456cabc71a57f48313f33734aebc1a66ce07dc
Julius A. Odai <jlsodai@gmail.com>
2024-06-19 16:19:13 +0000

    update layout

--- app/(home)/layout.js
+++ app/(home)/layout.js
@@ -2,12 +2,20 @@ import Navigation from "@/app/sections/Navigation";
 import FooterSection from "@/app/sections/FooterSection";
 import FooterMenu from "@/app/sections/FooterMenu";
 export default function HomePageLayout({ children }) {
-	return (
-		<>
-			<Navigation />
-			{children}
-			<FooterSection />
-			<FooterMenu />
-		</>
-	);
+  return (
+    <>
+      {/* <div className="bg-tender">
+        <div className="container py-3 text-white">
+          TheBoardroom Africa membership now open to male candidates.{" "}
+          <a href="#" className="text-mustard">
+            Read CEO address.
+          </a>
+        </div>
+      </div> */}
+      <Navigation />
+      {children}
+      <FooterSection />
+      <FooterMenu />
+    </>
+  );
 }
