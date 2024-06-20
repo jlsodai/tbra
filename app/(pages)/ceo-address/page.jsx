e2fdf8fd5f342153c80c41ee9826cf09530a8662
Julius A. Odai <jlsodai@gmail.com>
2024-06-20 08:52:36 +0000

    update address

--- app/(pages)/ceo-address/page.jsx
+++ app/(pages)/ceo-address/page.jsx
@@ -18,7 +18,7 @@ const page = () => {
 
           <img
             src="https://res.cloudinary.com/tbra/image/upload/v1680706201/marcia_yzavpl.jpg"
-            width="200px"
+            width="250px"
             className="float-left mr-8 mb-8 mt-0"
             alt=""
           />
