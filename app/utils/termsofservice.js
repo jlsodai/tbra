b506c46ac7afda4d739f47c0f4f4cff35b708a6b
Julius A. Odai <jlsodai@gmail.com>
2024-08-26 09:46:18 +0000

    add bold to terms of service

--- app/utils/termsofservice.js
+++ app/utils/termsofservice.js
@@ -1,9 +1,5 @@
 export const termsofservice = `
-TERMS OF SERVICE
-
-----
-
-OVERVIEW
+**OVERVIEW**
 
 This website is operated by TheBoardroom Africa. Throughout the site, the terms “we,” “us,” and “our” refer to TheBoardroom Africa. TheBoardroom Africa offers this website, including all information, tools, and Services (defined below) available from this site to you, the user, upon your acceptance of all terms, conditions, policies, and notices stated herein.
 
@@ -13,7 +9,7 @@ Please read these Terms carefully before accessing or using our Services on our 
 
 Any new features or tools which are added to the current website are subject to the Terms of Service. You can review the most current version of the Terms of Service at any time on this page. We reserve the right to update, change or replace any part of these Terms of Service by posting updates and/or changes to our website. It is your responsibility to check this page periodically for changes. Your continued use of or access to the website following the posting of any changes constitutes your acceptance of those changes.
 
-SECTION 1 - WEBSITE TERMS
+**SECTION 1 - WEBSITE TERMS**
 
 By agreeing to these Terms of Service, you represent that you are at least the age of majority in your state or province of residence, or that you are the age of majority in your state or province of residence, and you have given us your consent to allow any of your minor dependents to use this site.
 
@@ -23,7 +19,7 @@ You must not transmit any worms or viruses or any code of a destructive nature.
 
 A breach or violation of any of the Terms will result in an immediate termination of your Services.
 
-SECTION 2 - GENERAL CONDITIONS
+**SECTION 2 - GENERAL CONDITIONS**
 
 We reserve the right to refuse service to anyone for any reason at any time.
 
@@ -33,19 +29,19 @@ You agree not to reproduce, duplicate, copy, sell, resell or exploit any portion
 
 The headings used in this agreement are included for convenience only and will not limit or otherwise affect these Terms.
 
-SECTION 3 - ACCURACY, COMPLETENESS AND TIMELINESS OF INFORMATION
+**SECTION 3 - ACCURACY, COMPLETENESS AND TIMELINESS OF INFORMATION**
 
 We are not responsible if information made available on this site is not accurate, complete, or current. The material on this site is provided for general information only and should not be relied upon or used as the sole basis for making decisions without consulting primary, more accurate, more complete, or more timely sources of information. Any reliance on the material on this site is at your own risk.
 
 This site may contain certain historical information. Historical information, necessarily, is not current and is provided for your reference only. We reserve the right to modify the contents of this site at any time, but we have no obligation to update any information on our site. You agree that it is your responsibility to monitor changes to our site.
 
-SECTION 4 - MODIFICATIONS TO THE SERVICE AND PRICES
+**SECTION 4 - MODIFICATIONS TO THE SERVICE AND PRICES**
 
 Prices for our products are subject to change without notice. We reserve the right to modify or discontinue the Services (or any part or content thereof) without notice at any time.
 
 We will not be liable to you or to any third-party for any modification, price change, suspension, or discontinuance of the Service.
 
-SECTION 5 - PRODUCTS OR SERVICES (if applicable)
+**SECTION 5 - PRODUCTS OR SERVICES (if applicable)**
 
 Certain products or Services may be available exclusively online through the website. These products or Services may have limited quantities. All sales are final.
 
@@ -53,25 +49,25 @@ We reserve the right, but are not obligated, to limit the sales of our products 
 
 We do not warrant that the quality of any products, Services, information, or other material purchased or obtained by you will meet your expectations, or that any errors in the Service will be corrected.
 
-SECTION 6 - ACCURACY OF BILLING AND ACCOUNT INFORMATION
+**SECTION 6 - ACCURACY OF BILLING AND ACCOUNT INFORMATION**
 
 We reserve the right to refuse any order you place with us. We may, in our sole discretion, limit or cancel quantities purchased per person, per household, or per order. These restrictions may include orders placed by or under the same customer account, the same credit card, and/or orders that use the same billing and/or shipping address. In the event that we make a change to or cancel an order, we may attempt to notify you by contacting the e-mail and/or billing address/phone number provided at the time the order was made. We reserve the right to limit or prohibit orders that, in our sole judgment, appear to be placed by dealers, resellers or distributors.
 
 You agree to provide current, complete, and accurate purchase and account information for all purchases made on our site. You agree to promptly update your account and other information, including your email address and credit card numbers and expiration dates, so that we can complete your transactions and contact you as needed.
 
-SECTION 7 - OPTIONAL TOOLS
+**SECTION 7 - OPTIONAL TOOLS**
 
 We may provide you with access to third-party tools over which we neither monitor nor have any control nor input. You acknowledge and agree that we provide access to such tools ”as is” and “as available” without any warranties, representations or conditions of any kind and without any endorsement. We shall have no liability whatsoever arising from or relating to your use of optional third-party tools. Any use by you of the optional tools offered through the site is entirely at your own risk and discretion and you should ensure that you are familiar with and approve of the terms on which tools are provided by the relevant third-party provider(s).
 
 We may also, in the future, offer new Services and/or features through the website (including, the release of new tools and resources). Such new features and/or Services will be subject to these Terms of Service.
 
-SECTION 8 - THIRD-PARTY LINKS
+**SECTION 8 - THIRD-PARTY LINKS**
 
 Certain content, products and Services available via our Service may include materials from third-parties. Third-party links on this site may direct you to third-party websites that are not affiliated with us. We are not responsible for examining or evaluating the content or accuracy and we do not warrant and will not have any liability or responsibility for any third-party materials or websites, or for any other materials, products, or Services of third-parties.
 
 We are not liable for any harm or damages related to the purchase or use of goods, Services, resources, content, or any other transactions made in connection with any third-party websites. Please review carefully the third-party's policies and practices and make sure you understand them before you transact. Complaints, claims, concerns, or questions regarding third-party products should be directed to the third-party.
 
-SECTION 9 - USER COMMENTS, FEEDBACK, GRANT, AND CONDITIONS
+**SECTION 9 - USER COMMENTS, FEEDBACK, GRANT, AND CONDITIONS**
 
 If you choose to provide feedback including any ideas, suggestions, concepts, processes, techniques, questions, answers, codes/scripts, and other comments related to our Services, site, proposed services, documentation or business (“Feedback”), we will own and may use such Feedback without any restrictions and obligations to you, and you hereby waive any claim you have to ownership, compensation, monetary or otherwise.
 
@@ -81,17 +77,17 @@ We may, but have no obligation to, monitor, edit or remove content that we deter
 
 You agree that your comments will not violate any right of any third-party, including copyright, trademark, privacy, personality or other personal or proprietary right. You further agree that your comments will not contain libelous or otherwise unlawful, abusive, or obscene material, or contain any computer virus or other malware that could in any way affect the operation of the Service or any related website. You may not use a false e-mail address, pretend to be someone other than yourself, or otherwise mislead us or third-parties as to the origin of any comments. You are solely responsible for any comments you make and their accuracy. We take no responsibility and assume no liability for any comments posted by you or any third-party.
 
-SECTION 10 - PERSONAL INFORMATION
+**SECTION 10 - PERSONAL INFORMATION**
 
 Your submission of personal information through the store is governed by our Privacy Policy. To view our Privacy Policy, please see [LINK TO PRIVACY POLICY](https://theboardroomafrica.com/privacy-policy)
 
-SECTION 11 - ERRORS, INACCURACIES AND OMISSIONS
+**SECTION 11 - ERRORS, INACCURACIES AND OMISSIONS**
 
 Occasionally there may be information on our site or in the Service that contains typographical errors, inaccuracies or omissions that may relate to product descriptions, pricing, promotions, offers, product shipping charges, transit times and availability. We reserve the right to correct any errors, inaccuracies, or omissions, and to change or update information or cancel orders if any information in the Service or on any related website is inaccurate at any time without prior notice (including after you have submitted your order).
 
 We undertake no obligation to update, amend or clarify information in the Service or on any related website, including without limitation, pricing information, except as required by law. No specified update or refresh date applied in the Service or on any related website, should be taken to indicate that all information in the Service or on any related website has been modified or updated.
 
-SECTION 12 - PROHIBITED USES
+**SECTION 12 - PROHIBITED USES**
 
 In addition to other prohibitions as set forth in the Terms of Service, following general conditions apply to your use of the Services:
 
@@ -124,7 +120,7 @@ You are responsible for your conduct and activity when using our website. You mu
 
 We reserve the right to terminate your use of the Service or any related website for violating any of the prohibited uses.
 
-SECTION 13 - DISCLAIMER OF WARRANTIES; LIMITATION OF LIABILITY
+**SECTION 13 - DISCLAIMER OF WARRANTIES; LIMITATION OF LIABILITY**
 
 We do not guarantee, represent, or warrant that your use of our service will be uninterrupted, timely, secure or error-free. We do not warrant that the results that may be obtained from the use of the service will be accurate or reliable.
 
@@ -139,18 +135,18 @@ Because some states or jurisdictions do not allow the exclusion or the limitatio
 
 
 
-SECTION 14 - INDEMNIFICATION
+**SECTION 14 - INDEMNIFICATION**
 
 You agree to indemnify, defend and hold harmless TheBoardroom Africa and our parent, subsidiaries, affiliates, partners, officers, directors, agents, contractors, licensors, service providers, subcontractors, suppliers, interns and employees, harmless from any claim or demand, including reasonable attorneys’ fees, made by any third-party due to or arising out of your breach of these Terms of Service or the documents they incorporate by reference, or your violation of any law or the rights of a third-party.
 
 
 
 
-SECTION 15 - SEVERABILITY
+**SECTION 15 - SEVERABILITY**
 
 In the event that any provision of these Terms of Service is determined to be unlawful, void or unenforceable, such provision will nonetheless be enforceable to the fullest extent permitted by applicable law, and the unenforceable portion shall be deemed to be severed from these Terms of Service, such determination shall not affect the validity and enforceability of any other remaining provisions.
 
-SECTION 16 - TERMINATION
+**SECTION 16 - TERMINATION**
 
 The obligations and liabilities of the parties incurred prior to the termination date will survive the termination of this agreement for all purposes.
 
@@ -158,7 +154,7 @@ These Terms of Service are effective unless and until terminated by either you o
 
 If in our sole judgment you fail, or we suspect that you have failed, to comply with any term or provision of these Terms of Service, we also may terminate this agreement at any time without notice and you will remain liable for all amounts due up to and including the date of termination; and/or accordingly may deny you access to our Services (or any part thereof).
 
-SECTION 17 - ENTIRE AGREEMENT
+**SECTION 17 - ENTIRE AGREEMENT**
 
 The failure of us to exercise or enforce any right or provision of these Terms of Service does not constitute a waiver of such right or provision.
 
@@ -166,17 +162,17 @@ These Terms of Service and any policies or operating rules posted by us on this 
 
 Any ambiguities in the interpretation of these Terms of Service shall not be construed against the drafting party.
 
-SECTION 18 - GOVERNING LAW
+**SECTION 18 - GOVERNING LAW**
 
 These Terms of Service and any separate agreements whereby we provide you Services shall be governed by and construed in accordance with the laws of Mauritius.
 
-SECTION 19 - CHANGES TO TERMS OF SERVICE
+**SECTION 19 - CHANGES TO TERMS OF SERVICE**
 
 You can review the most current version of the Terms of Service at any time at this page.
 
 We reserve the right, at our sole discretion, to update, change or replace any part of these Terms of Service by posting updates and changes to our website. It is your responsibility to check our website periodically for changes. Your continued use of or access to our website or the Service following the posting of any changes to these Terms of Service constitutes acceptance of those changes.
 
-SECTION 20 - CONTACT INFORMATION
+**SECTION 20 - CONTACT INFORMATION**
 
 Questions about the Terms of Service should be sent to us at [info@theboardroomafrica.com](mailto:info@theboardroomafrica.com).
 
@@ -184,7 +180,7 @@ Our contact information is posted below:
 
 6th Floor, Tower A, 1 CyberCity, Ebene, Mauritius
 
-SECTION 21 - NO REFUND CLAUSE
+**SECTION 21 - NO REFUND CLAUSE**
 
 We do not offer refunds for any products or services purchased through our website unless explicitly stated otherwise. All sales are final.
 `;
