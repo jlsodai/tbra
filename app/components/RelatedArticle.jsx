d279ab547e0abed8bd64b34beffe8cfe34abd31c
Julius A. Odai <jlsodai@gmail.com>
2024-10-01 15:05:39 +0000

    adjust malado image

--- app/components/RelatedArticle.jsx
+++ app/components/RelatedArticle.jsx
@@ -14,14 +14,14 @@ const articles = [
     title: "2024 International Finance & Development Industry Trends",
     url: "/insights/member-insights-2024-international-finance-and-development-trends",
     image:
-      "https://res.cloudinary.com/dhhw72iwq/image/upload/v1711449462/malado_kxil5y.jpg",
+      "https://res.cloudinary.com/dhhw72iwq/image/upload/v1727795111/maladoBig_c0givf.jpg",
   },
   {
     id: 3,
     title: "2024 2024 Medical Industry Trends",
     url: "/insights/member-insights-2024-medical-industry-trends",
     image:
-      "https://res.cloudinary.com/dhhw72iwq/image/upload/w_650,h_800,c_fill,g_north/v1727791965/totimeh1_tlpuyk.jpg",
+      "https://res.cloudinary.com/dhhw72iwq/image/upload/w_650,h_600,c_fill,g_north/v1727791965/totimeh1_tlpuyk.jpg",
   },
 ];
 
